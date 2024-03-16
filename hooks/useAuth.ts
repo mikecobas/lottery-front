41c7a084295e722a7a6e8babb00a021f2bb807c2
AsaedReyes <asaed.reyes@kubofinanciero.com>
2024-03-16 11:18:55 -0600

    feat: :sparkles: Update (useAuth)
    Se actualiza logica de useauth

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,7 +1,6 @@
 import { AuthContext } from "@/contexts/AuthContext";
 import { useState, useContext } from "react";
-import { AuthResponse, Payload, User } from "@/interfaces/auth.interface"; // Asegúrate de que la ruta de importación sea correcta
-
+import { AuthResponse, Payload, User } from "@/interfaces/auth.interface";
 const useAuth = () => {
   const { dispatch } = useContext(AuthContext);
 

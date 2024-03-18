eac7e5cad5f29e236deff059a8aeac87a3d759fd
AsaedReyesMedina <asaed.reyes@kubofinanciero.com>
2024-03-18 02:28:07 -0600

    feat: :sparkles: Update(premios y sorteos)
    Se actualiza logica para que solo muestre contenido de las tablas creado por el usurio

--- components/Tables/TableHomePremios.tsx
+++ components/Tables/TableHomePremios.tsx
@@ -6,6 +6,7 @@ import { useContext, useEffect, useState } from 'react';
 import ModalCrudPremios from '../Modals/ModalCrudPremios';
 import { ModalDelete } from '../Modals/ModalDelete';
 import { User } from '@/interfaces/auth.interface';
+import { ContestContext } from '@/contexts/ContestContext';
 type action = 'edit' | 'create'
 export function TableHomePremios() {
     const [localStorageUser, setLocalStorageUser] = useState<User>();
@@ -20,12 +21,26 @@ export function TableHomePremios() {
         }
     }, []);
     const { state } = useContext(PrizesContext);
+    const [nombresSorteos, setNombresSorteos] = useState<any[]>([]);
+    const [sorteos, setSorteos] = useState<any[]>([]);
+    const { state: stateContest } = useContext(ContestContext);
     const [action, setaction] = useState<action>("create")
     const [openModalEdit, setOpenModalEdit] = useState(false)
     const [openModalDelete, setOpenModalDelete] = useState(false)
     const [data, setData] = useState<any>()
+    useEffect(() => {
+        let tempNombresSorteos: any = []
+        let tempSorteos: any = []
+        stateContest.payload.map((sorteo) => {
+            if (sorteo.createdBy.name === localStorageUser?.name) {
+                tempNombresSorteos.push({ label: sorteo.name, value: sorteo._id })
+            }
+        })
+        setNombresSorteos(tempNombresSorteos);
+        setSorteos(tempSorteos);
+    }, [state, localStorageUser])
     const rows = state.payload
-        .filter(prizes => prizes.status === true)
+        .filter(prizes => prizes.status === true && nombresSorteos.some(sorteo => sorteo.value === prizes.contestId?._id))
         .map((prizes, index) => (
             <Table.Tr key={index}>
                 <Table.Td>{prizes.name}</Table.Td>
